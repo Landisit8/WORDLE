47800aaafcda3f3a329c88b05f2caaac2ef03021
Landisit8 <federicolandini8@gmail.com>
2023-04-11 18:54:24 +0200

    Costruzione lato server
    Creazione delle prime classi

---
+++ src/Memory.java
@@ -0,0 +1,4 @@
+
+// Classe Memory -> Memorizzare tutti gli utenti, gestione delle statistiche, classifica ordinata, traduzione italiana
+public class Memory {
+}
---
+++ src/ServerMain.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+// Classe serverMain -> Parsing, Iterazione con i client, creazione della nuova parola, iteazioni con client
+public class ServerMain {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);   //  Scanner
+        Register register;  //  Registrazione
+        Login login;    //  Login
+        boolean inputOk = false;    //  Controllo input
+        String input;
+        String stringa;
+
+        int Inserimento;
+
+        do{
+            Inserimento= scanner.nextInt();
+        switch (Inserimento){
+            case 0:
+                System.out.println("Registrazione, Inserisci l'username");
+                stringa = scanner.next();
+                System.out.println("Inserisci la password");
+                input = scanner.next();
+                register = new Register(stringa, input);
+                break;
+            case 1:
+                System.out.println("Login");
+                System.out.println("Inserisci l'username");
+                stringa = scanner.next();
+                System.out.println("Inserisci la password");
+                input = scanner.next();
+                inputOk = true;
+                break;
+        }
+        }while(inputOk!=true);
+    }
+}
